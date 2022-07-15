547752033c9263083b4619171da124e0df6fb405
Drew Hollenberg <hollenbergdrew@gmail.com>
2022-07-14 17:43:16 -0700

    Added the iframe for Map

--- client/src/Map.js
+++ client/src/Map.js
@@ -12,6 +12,15 @@ function Map() {
     <main className="container">
       <h1>San Francisco Public Parking</h1>
       <h4 className="header">Map View</h4>
+      <iframe
+        allow="geolocation"
+        src="https://data.sfgov.org/Transportation/SFMTA-Map/hwpu-syk3/embed?width=1000&height=625"
+        width="1000"
+        height="625"
+        className="Map"></iframe>
+      <br />
+      <br />
+
       <div className="end">
         <a
           href="https://www.sfmta.com/garages-lots?field_garage_services_value=All&field_neighborhoods_target_id=All&field_parking_type_value=All#views-exposed-form-garage-map-block"
